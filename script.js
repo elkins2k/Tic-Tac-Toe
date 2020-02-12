2a5d549008128be49a1f4feeb92713b1a48ee9ea
Jeff Elkins <elkins2k@mail.com>
2020-02-12 14:03:11 -0600

    commit8

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let turn=`red`
+let turn=`RED`
 let counter=0
 let gridClick=`some string`
 
@@ -8,25 +8,66 @@ instructions.innerText = newBoard
 
 const resetButton = document.querySelector(`#resetButton`)
 resetButton.addEventListener (`click`, () => {
-    turn=`red`
+    turn=`RED`
     counter=0
     instructions.innerText = newBoard
+    redMoves=[]
+    blueMoves=[]
+    won=false
     return gridBlocks.forEach (gridBlock => {
         gridBlock.setAttribute (`style` , `background: white`)
         gridBlock.addEventListener(`click`, addColor)
     })
 })
 
-function addColor () {
-    gridClick = document.querySelector(`#${event.target.id}`)
-    gridClick.setAttribute(`style`, `background: ${turn}`)
-    gridClick.removeEventListener(`click`,addColor)
-    if (counter === 8) {
-        instructions.innerText = `Out of moves. Select the RESET to continue.`        
-    } else {
-        counter ++
-        turn = counter %2 === 0 ? `red` : `blue`
-        instructions.innerText = `It is now ${turn}'s turn...`
+const winningSequences = [
+    [`11`,`12`,`13`],[`21`,`22`,`23`],[`31`,`32`,`33`], //across
+    [`11`,`21`,`31`],[`12`,`22`,`32`],[`13`,`23`,`33`], //down
+    [`11`,`22`,`33`],[`13`,`22`,`31`] //diagonal
+]
+let redMoves=[]
+redWins=0
+const redScore = document.querySelector(`#redScore`)
+let blueMoves=[]
+blueWins=0
+const blueScore = document.querySelector(`#blueScore`)
+let won = false
+function checkWinner (moves) {
+    winningSequences.forEach ( sequence => {
+        if (sequence.every (winner => moves.includes(winner))){
+            won = true;
+            instructions.innerText = `${turn} won! Select the RESET to continue.`;
+            if (turn === `RED`) {
+                redWins++
+                redScore.innerText = `Red Score: ${redWins}`
+            } else {
+                blueWins++
+                blueScore.innerText = `Blue Score: ${blueWins}`
+            }
+        }
+    })
+}
+
+function addColor (event) {
+    //gridClick = document.querySelector(`#${event.target.id}`)
+    if (won===false) {
+        gridClick = document.getElementById(`${event.target.id}`)
+        gridClick.setAttribute(`style`, `background: ${turn}`)
+        gridClick.removeEventListener(`click`,addColor)
+        if (turn === `RED`) {
+            redMoves.push(gridClick.id)
+            checkWinner(redMoves)
+        } else {
+            blueMoves.push(gridClick.id)
+            checkWinner(blueMoves)
+        }
+        if (counter === 8 && won === false) {
+            instructions.innerText = `It's a tie! Select the RESET to continue.`        
+        } else if ( won === false ){
+            counter ++
+            turn = counter %2 === 0 ? `RED` : `BLUE`
+            instructions.innerText = `It is now ${turn}'s turn...`
+        }
     }
 }
 
