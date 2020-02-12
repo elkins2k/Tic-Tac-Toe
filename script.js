3771c7bb5353605752a5ac227922264a46bc0491
Jeff Elkins <elkins2k@mail.com>
2020-02-12 11:28:31 -0600

    commit7

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const gridBlocks = document.querySelectorAll(`.gridBlock`)
-const redBlocks=0
-const blueBlocks=0
 let turn=`red`
-let counter=redBlocks+blueBlocks
-let gridClick=``
+let counter=0
+let gridClick=`some string`
+
 const instructions = document.querySelector(`instructions`)
 const newBoard = `Select a square to go first`
 instructions.innerText = newBoard
 
 const resetButton = document.querySelector(`#resetButton`)
 resetButton.addEventListener (`click`, () => {
-    instructions.innerText = newBoard
-    counter=0
     turn=`red`
-    gridBlocks.forEach (gridBlock => {
+    counter=0
+    instructions.innerText = newBoard
+    return gridBlocks.forEach (gridBlock => {
         gridBlock.setAttribute (`style` , `background: white`)
+        gridBlock.addEventListener(`click`, addColor)
     })
 })
 
 function addColor () {
     gridClick = document.querySelector(`#${event.target.id}`)
     gridClick.setAttribute(`style`, `background: ${turn}`)
-    counter ++
-    turn = counter %2 === 0 ? `red` : `blue`
-    instructions.innerText = `It is now ${turn}'s turn...`
     gridClick.removeEventListener(`click`,addColor)
+    if (counter === 8) {
+        instructions.innerText = `Out of moves. Select the RESET to continue.`        
+    } else {
+        counter ++
+        turn = counter %2 === 0 ? `red` : `blue`
+        instructions.innerText = `It is now ${turn}'s turn...`
+    }
 }
 
+const gridBlocks = document.querySelectorAll(`.gridBlock`)
 gridBlocks.forEach (gridBlock => {
-    if (counter < 9) {
-        gridBlock.addEventListener(`click`, addColor)
-    } else {
-    instructions.innerText = `Out of moves. Select the RESET to continue.`
-    }
+    gridBlock.addEventListener(`click`, addColor)    
 })
